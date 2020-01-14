ed6f5014948bed7a7faa169e1ffa18553a99f4b5
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2020-01-14 16:51:11 +0100

    Removed unnecessary using

--- QuizManager/Pages/Logout.cshtml.cs
+++ QuizManager/Pages/Logout.cshtml.cs
@@ -1,7 +1,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace QuizManager.Pages
